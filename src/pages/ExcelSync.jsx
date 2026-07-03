ee06aa449d9a1df03a06bd29ee0180531077a16b
kevinguillen827-eng <kevinguillen827@gmail.com>
2026-07-03 01:40:14 -0500

    Update ExcelSync.jsx

--- src/pages/ExcelSync.jsx
+++ src/pages/ExcelSync.jsx
@@ -23,8 +23,8 @@ export default function ExcelSync() {
     }
   };
 
-  const handleDisconnect = () => {
-    disconnectFile();
+  const handleDisconnect = async () => {
+    await disconnectFile();
     setSyncedFile(null);
   };
 
